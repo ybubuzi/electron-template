5b295df4987f636ffb93b84407bd231839185e9c
bubuzi <bubuzi@88.com>
2023-07-26 11:18:26 +0800

    添加程序自启功能

---
+++ src/main/utils/BootstrapUtils.ts
@@ -0,0 +1,47 @@
+import WinReg from 'winreg'
+
+const RUN_LOCATION = '\\Software\\Microsoft\\Windows\\CurrentVersion\\Run'
+type RegCallback = (err: Error) => void
+// 获取注册表
+const getKey = () => {
+    return new WinReg({
+        hive: WinReg.HKCU,
+        key: RUN_LOCATION
+    })
+}
+// callback自定义方法，你可以在这里写代码
+const nopp: RegCallback = (_: Error) => { }
+
+/**
+ * 设置程序自动启动
+ * @param name 程序名称
+ * @param exePath 执行文件路径
+ * @param callback 
+ */
+export const enableAutoStart = (name: string, exePath: string, callback?: RegCallback) => {
+    const kye = getKey()
+    kye.set(name, WinReg.REG_SZ, exePath, callback || nopp)
+}
+
+/**
+ * 禁用自动启动
+ * @param name 程序名称
+ * @param callback 
+ */
+export const disableAutoStart = (name: string, callback?: RegCallback) => {
+    const kye = getKey()
+    kye.remove(name, callback || nopp)
+}
+
+/**
+ * 判断是否自动启动
+ * @param name 
+ * @param callback 
+ * @returns 
+ */
+export const getAutoStartValue = (name: string, callback?: RegCallback): boolean => {
+    const kye = getKey()
+    const result = kye.get(name, callback || nopp)
+    if (!result) return false
+    return result.values[0].value === '1'
+}
