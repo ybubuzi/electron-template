2ac0bbb2117277dd85dfadef38ce1dfd80c6abde
Robin Boyer <bubuzi@88.com>
2023-08-15 15:17:43 +0800

    pref:优化打包配置

--- src/main/index.ts
+++ src/main/index.ts
@@ -66,16 +66,18 @@ function createWindow(): void {
 }
 
 app.whenReady().then(() => {
-  // @ts-ignore 注册程序id
-  electronApp.setAppUserModelId(import.meta.env.APP_ID)
 
-  session.defaultSession.loadExtension(join(__dirname, `../../devtool`), { allowFileAccess: true })
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  // 开发环境下加载VUE devtool
+  if (is.dev) {
+    session.defaultSession.loadExtension(join(__dirname, `../../devtool`), { allowFileAccess: true })
+    // @ts-ignore 设置程序ID,仅在开发模式下生效
+    electronApp.setAppUserModelId(import.meta.env.APP_ID)
+  } else {
+    // 注册运行程序
+    app.setAppUserModelId(process.execPath)
+  }
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  // 用于屏蔽F12与`Ctrl+Shift+I`快捷键打开开发者工具
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
